cab9d8a4d457fffbba02ae84361a17aa04eb4a99
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-03-14 09:45:51 +0400

    Include db.local.php if it exists in config

--- config/db.php
+++ config/db.php
@@ -1,6 +1,9 @@
 <?php
-
-return [
+$config = [];
+if (file_exists(__DIR__.'/db.local.php')){
+    $config = require_once __DIR__.'/db.local.php';
+}
+return array_merge([
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=vuejs_yii2_notes',
     'username' => 'root',
@@ -11,4 +14,4 @@ return [
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
-];
+], $config);
